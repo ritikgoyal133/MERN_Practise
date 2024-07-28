fb5f832ea26bf27c79c6d7508a50ddec67a8ea4f
Ritik Goyal <ritikgoyal3092000@gmail.com>
2024-07-28 14:25:24 +0530

    basic crud api's

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import http from "http";
 import fs from "fs";
 import express from "express";
+import morgan from "morgan";
 // const express = require('express')
 
 const app = express(); //This is an instance of an Express.js application.
@@ -43,20 +44,67 @@ try {
 // })
 
 //API-endpoint-route
-app.get("/", (req, res) => {
-  res.send("Hello, GET request!");
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+//Read Get products
+//CRUD
+
+//Read
+app.get("/products", (req, res) => {
+  // res.send("Hello, GET request!");
+  // res.json(products);
+  res.json({"name":"Ram"});
+  //res.json() is a method in Express.js that sends a JSON response to the client. It is a convenience method that automatically sets the Content-Type header of the response to application/json and sends the response body as a JSON string.
 });
 
-app.post("/", (req, res) => {
-  res.send("Hello, POST request!");
+//Get perticular product
+app.get("/products/:id", (req, res) => {
+  console.log(req.params);
+  const id = +req.params.id;
+  const product = products.find((product) => product.id === id);
+  res.json(product);
 });
 
-app.put("/user", (req, res) => {
-  res.send("Hello, PUT request!");
+//POST Create Product
+//Create
+app.post('/products', (req, res) => {
+  console.log(req.body);
+  products.push(req.body);
+
+  const writableStream = fs.createWriteStream('output.txt');
+  writableStream.write(JSON.stringify(products, null, 2)); // Convert array to JSON string
+  // JSON.stringify(products, null, 2) to convert the products array to a JSON string. The second parameter null and the third parameter 2 are used for pretty-printing the JSON with an indentation of 2 spaces.
+  writableStream.end();
+
+  writableStream.on('finish', () => {
+    // res.json(req.body);
+    res.status(201).json(req.body);
+  });
+
+  writableStream.on('error', (err) => {
+    console.error('Error:', err);
+    res.status(500).send('Internal Server Error');
+  });
 });
 
-app.delete("/user", (req, res) => {
-  res.send("Hello, DELETE request!");
+//Update the product
+//PUT (Update an existing resource)
+app.put("/products/:id", (req, res) => {
+  console.log(req.body);
+  res.status(200).json({"mesage":"Hello, PUT request!"});
+});
+
+//Update the product
+//PATCH (Partially update an existing resource)
+app.patch("/products/:id", (req, res) => {
+  console.log(req.body);
+  res.status(200).json({"mesage":"Patch Request, data updated partially!"});
+});
+
+app.delete("/products/:id", (req, res) => {
+  res.status(200).send({"message":"Hello, DELETE request!"});
 });
 
 app.listen(8080, () => {
